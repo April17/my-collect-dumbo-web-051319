e6ea64dfc7b7afae312f0a9cb6ccff327ef9e49b
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-27 03:25:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 require "pry"
 def my_collect (array)
-  array do |name|
-  name.split(" ").first
-end
-  array do |lang|
-  lang.upcase
-end
-
+    array do |name|
+      name.split(" ").first
+    end
+    array do |lang|
+      lang.upcase
+    end
 end
