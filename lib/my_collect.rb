b8ddc905b32879254f23e0c6accedd22129bac3b
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-27 03:42:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,15 @@
 require "pry"
-def my_collect (array)
+def my_collect (array1,array2)
   i = 0
   ary1 = []
   ary2 = []
-  while i < array.length
+  while i < array1.length
       ary1 << yield(array[i]).upcase
       i += 1
   end
   ary1
   i = 0
-  while i < array.length
+  while i < array2.length
       ary2 << yield(array[i]).split(" ").first
       i += 1
   end
