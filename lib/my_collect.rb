9908bd27ca53e9d9ea819ce1043f121feb70cbcd
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-27 04:21:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,12 +2,9 @@ require "pry"
 def my_collect (array)
   i = 0
   ary1 = []
-  ary2 = []
   while i < array.length
-    ary2 << yield(array[i])
     ary1 << yield(array[i])
     i += 1
   end
   ary1
-  ary2
 end
