bb2fe23045b188de4df7d6f72923ce85779c19e0
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-27 03:29:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 require "pry"
 def my_collect (array)
-    array do |name|
-      name.split(" ").first
-    end
-    array do |lang|
-      lang.upcase
-    end
+  i = 0
+  ary1 = []
+  ary2 = []
+  while i < array.length
+    ary1 = yield(array[i]).upcase
+    ary2 = yield(array[i]).split(" ").first
+    i += 1
+  end
 end
