8793559ed5a4341f1f98bda6c0cc00be85977858
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-27 03:48:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,18 +3,15 @@ def my_collect (array)
   i = 0
   ary1 = []
   ary2 = []
-  if array.length == 4
-    while i < array.length
+  while i < array.length
       ary1 << yield(array[i]).upcase
       i += 1
-    end
-    return ary1
-  elsif array.length == 3
-    i = 0
-    while i < array.length
+  end
+  ary1
+  i = 0
+  while i < array.length
       ary2 << yield(array[i]).split(" ").first
       i += 1
-    end
-    return ary2
   end
+  ary2
 end
