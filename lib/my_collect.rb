74816471453ed630395a0027ebeb66a6dbb7921c
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-27 03:46:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,15 +3,18 @@ def my_collect (array)
   i = 0
   ary1 = []
   ary2 = []
-  while i < array.length
+  if array.length == 4
+    while i < array.length
       ary1 << yield(array[i]).upcase
       i += 1
-  end
-  ary1
-  i = 0
-  while i < array.length
+    end
+    ary1
+  elsif array.length == 3
+    i = 0
+    while i < array.length
       ary2 << yield(array[i]).split(" ").first
       i += 1
+    end
+    ary2
   end
-  ary2
 end
