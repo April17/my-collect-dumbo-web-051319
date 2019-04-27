4dc87d160be3cb184763b3672bebda5a2d8d21c2
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-27 03:42:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,13 +4,13 @@ def my_collect (array1,array2)
   ary1 = []
   ary2 = []
   while i < array1.length
-      ary1 << yield(array[i]).upcase
+      ary1 << yield(array1[i]).upcase
       i += 1
   end
   ary1
   i = 0
   while i < array2.length
-      ary2 << yield(array[i]).split(" ").first
+      ary2 << yield(array2[i]).split(" ").first
       i += 1
   end
   ary2
