622b72e53e2d96e7f31bb1f4d77698b0dd050565
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-27 02:53:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+require "pry"
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect (array)
+  yield (array)
+  yield (array)
+end
+my_collect (languages) do |lang|
+  puts lang.upcase
+end
+my_collect(students) do |name|
+  puts name.split(" ").first
+end
